c65b50adfd21aa9aa11ff2dd340006c93ad13ece
github username <suoarik@gmail.com>
2019-02-01 20:07:02 +0000

    added semantic ui styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import FreeCodeCampBegAlgo from "./components/codecampbeginner/CelsiusConvert"
+import ThirtyDayChallenge from 'components/codecampbeginner/ThirtyDayChallenge'
+// import 'semantic-ui-css/semantic.min.css'
+
+import { Grid } from "semantic-ui-react"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>Coding Sandbox</header>
-        <FreeCodeCampBegAlgo />
 
-      </div>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={16} >
+              <header>Coding Sandbox</header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={8} >
+              <FreeCodeCampBegAlgo />
+            </Grid.Column>
+            <Grid.Column width={8} >
+              <ThirtyDayChallenge />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+     
     )
   }
 }
---
+++ src/components/HackerRank/ThirtyDayCallenge.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ThirtyDayChallenge extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <p><button ></button></p> );
+    }
+}
+ 
+export default ThirtyDayChallenge;
